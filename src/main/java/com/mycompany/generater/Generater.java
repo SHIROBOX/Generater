e778b8eca2d5001d2fff0e2fea4e00a97584d288
ACHIRA <achira@geniustree.co.th>
2023-06-30 14:32:58 +0700

    init commit

---
+++ src/main/java/com/mycompany/generater/Generater.java
@@ -0,0 +1,12 @@
+package com.mycompany.generater;
+
+/**
+ *
+ * @author mbk_i
+ */
+public class Generater {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
